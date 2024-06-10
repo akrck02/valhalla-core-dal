ff661411f8c03935702f0cc2ae593503ad537873
akrck02 <aketza.vazquez@gmail.com>
2024-06-10 21:45:43 +0200

    team tests wip

--- tests/team_test.go
+++ tests/team_test.go
@@ -4,43 +4,52 @@ import (
 	"testing"
 
 	"github.com/akrck02/valhalla-core-dal/mock"
-	teamdal "github.com/akrck02/valhalla-core-dal/services/team"
-	"github.com/akrck02/valhalla-core-sdk/models"
 )
 
 func TestCreateTeam(t *testing.T) {
 
 	user := RegisterMockTestUser(t)
-
-	err := teamdal.CreateTeam(&models.Team{
-		Name:        mock.Name(),
-		Description: mock.Description(),
-		Owner:       user.ID,
-	})
-
-	if err != nil {
-		t.Error("error on create", err)
-	}
-
+	_ = CreateMockTestTeamWithOwner(t, user)
 	DeleteTestUser(t, user)
 
 }
 
 func TestEditTeam(t *testing.T) {
 
-	RegisterMockTestUser(t)
+	user := RegisterMockTestUser(t)
+	team := CreateMockTestTeamWithOwner(t, user)
 
+	team.Name = mock.TeamNameLong()
+	team.Description = mock.TeamDescriptionLong()
+	team = EditTestTeam(t, team)
+	obtainedTeam := GetTestTeam(t, team)
+
+	if obtainedTeam.Name != team.Name || obtainedTeam.Description != team.Description {
+		t.Errorf("Team changes not reflected in database")
+		return
+	}
+
+	t.Log("Team edited successfully")
+	DeleteTestUser(t, user)
 }
 
 func TestDeleteTeam(t *testing.T) {
 
-	RegisterMockTestUser(t)
-
+	user := RegisterMockTestUser(t)
+	team := CreateMockTestTeamWithOwner(t, user)
+	DeleteTestTeam(t, team)
+	DeleteTestUser(t, user)
 }
 
 func TestAddTeamMember(t *testing.T) {
 
-	RegisterMockTestUser(t)
+	user := RegisterMockTestUser(t)
+	member := RegisterMockTestUser(t)
+	team := CreateMockTestTeamWithOwner(t, user)
+	_ = AddTestTeamMember(t, team, member)
+	DeleteTestTeam(t, team)
+	DeleteTestUser(t, user)
+
 }
 
 func TestRemoveTeamMember(t *testing.T) {
