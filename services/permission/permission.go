41f85e4e1a89bc4365297d5d1a4fd99d6ca8d2c2
Itros97 <itros97@gmail.com>
2024-06-24 20:00:18 +0200

    added permission if project can be deleted by loged user

--- services/permission/permission.go
+++ services/permission/permission.go
@@ -1,6 +1,7 @@
 package permissiondal
 
 import (
+	projectmodels "github.com/akrck02/valhalla-core-sdk/models/project"
 	usersmodels "github.com/akrck02/valhalla-core-sdk/models/users"
 )
 
@@ -11,3 +12,17 @@ func CanEditUser(author *usersmodels.User, user *usersmodels.User) bool {
 func CanSeeUser(author *usersmodels.User, user *usersmodels.User) bool {
 	return author.Email == user.Email
 }
+func CanDeleteProject(logedUser *usersmodels.User, project *projectmodels.Project) bool {
+	return logedUser.ID == project.Owner
+}
+
+//Projects
+
+// func CanEditProject(author *models.Project, user *models.Project) bool {
+
+// 	return author.Email == user.Email
+// }
+
+// func CanSeeProject(author *models.Project, user *models.Project) bool {
+// 	return author.Email == user.Email
+// }
